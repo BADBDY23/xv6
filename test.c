0e44e9f3e029efeb002aad1221af42ac36eb8583
Mohammad <mohammadkarimi.ceit@gmail.com>
2015-12-21 10:11:11 +0330

    first test program on xv6

---
+++ test.c
@@ -0,0 +1,16 @@
+//simple test file :))
+
+#include "types.h"
+#include "user.h"
+#include "stat.h"
+
+void printf(int fd, char*s, ...)
+{
+	write(fd, s, strlen(s));
+}
+
+int main(void)
+{
+ printf(1, "first test ... :))\n");
+ exit();
+}
